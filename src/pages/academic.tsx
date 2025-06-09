bb353f6ff51979920b79b95fe357a90218672fe2
Argy1 <112714938+Argy1@users.noreply.github.com>
2025-06-09 20:12:40 +0700

    Update academic.tsx

--- src/pages/academic.tsx
+++ src/pages/academic.tsx
@@ -21,7 +21,7 @@ const timeline = [
 
 export default function Academic() {
   return (
-    <section className="px-8 md:px-24 pt-36 pb-24 bg-white dark:bg-gray-900 space-y-16">
+    <section className="px-8 md:px-24 pt-36 pb-24 space-y-24">
       <h1 className="text-4xl font-semibold text-gray-900 dark:text-gray-50 flex items-center gap-2">
         <Book /> Pengalaman Akademik
       </h1>
