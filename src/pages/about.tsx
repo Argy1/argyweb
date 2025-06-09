02dc1f7dd6370325aedf217dc2ced5641fe9ad12
Argy1 <112714938+Argy1@users.noreply.github.com>
2025-06-09 20:12:19 +0700

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 
 export default function About() {
   return (
-    <section className="px-8 md:px-24 pt-36 pb-24 bg-white dark:bg-gray-900 space-y-24">
+   <section className="px-8 md:px-24 pt-36 pb-24 space-y-24">
       <div className="space-y-6 max-w-3xl">
         <h1 className="text-4xl font-semibold text-gray-900 dark:text-gray-50 flex items-center gap-2">
           <Sparkles /> Tentang Website Ini
