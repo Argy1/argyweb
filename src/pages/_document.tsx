529c06678a22da57618f5ba9b1dff76bed553d48
Argy1 <112714938+Argy1@users.noreply.github.com>
2025-06-09 19:57:08 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* ← pastikan ada ini supaya mobile responsive */}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <body>
+      <Head />
+      <body
+        className="
+          antialiased font-sans
+          bg-white text-gray-900
+          dark:bg-gray-900 dark:text-gray-100
+        "
+      >
         <Main />
         <NextScript />
       </body>
