d516f80fc3482d61915a794d963975e9f26fa823
Argy1 <112714938+Argy1@users.noreply.github.com>
2025-06-09 19:10:30 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,17 @@
 // src/pages/_document.tsx
-import { Html, Head, Main, NextScript } from "next/document"
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased font-sans
-                       bg-white text-gray-900
-                       dark:bg-gray-900 dark:text-gray-100">
+      <Head>
+        {/* ← pastikan ada ini supaya mobile responsive */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
