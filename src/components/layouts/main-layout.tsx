a16cfdca5330fdf7d1eb2fdd31c4ecfbb4e747f3
Argy1 <112714938+Argy1@users.noreply.github.com>
2025-06-09 19:12:05 +0700

    Update main-layout.tsx

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <Navbar />
-      <main className="min-h-screen
+      <main className="pt-16 min-h-screen bg-[var(--bg)] text-[var(--fg)]"
                        bg-white text-gray-900
                        dark:bg-gray-900 dark:text-gray-100">
         {children}
